fec1b9cea17f74e397dfa466f2d852379c6ef02b
yzl123456 <376712116@qq.com>
2016-11-20 22:04:48 +0800

    add QRCode Demo and S_Key Demo

---
+++ InfoSafe-google-demo/src/testQRCode/TestQRCode.java
@@ -0,0 +1,25 @@
+package testQRCode;
+/**
+ * 其他2个类都是网上拉的，二维码的工具类，使用该二维码模块要先导入jar包，在lib目录下有，右键该jar包 build-path-->add to path
+	 * 生成二维码(内嵌LOGO)
+	 * 
+	 * @param content
+	 *            内容（即url的地址）
+	 * @param imgPath
+	 *            LOGO地址
+	 * @param destPath
+	 *            存放目录
+	 * @param needCompress
+	 *            是否压缩LOGO
+	 * @throws Exception
+	 public static void encode(String content, String imgPath, String destPath,
+			boolean needCompress) 
+ *   只需要调用上面函数，给4个参数就好了
+ */
+
+public class TestQRCode {
+	public static void main(String[] args) throws Exception {
+        String text = "http://www.baidu.com";
+        QRCodeUtil.encode(text, "h:/png/qq.png", "h:/png", true);
+    }
+}
